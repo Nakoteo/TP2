c1ee83fd87bc2d94f4d5eb4de112228f91b9afee
Rachel Boulay <boulayrachel.52@gmail.com>
2019-03-30 16:23:17 -0400

    ajustement animation

--- js/animation.js
+++ js/animation.js
@@ -3,7 +3,9 @@ window.addEventListener('load',function(){
     console.log('passe')
     let elmTitre = document.querySelector('.site-branding .site-title')
     let elmDescription = document.querySelector('.site-branding .site-description')
+    let elmNav = document.getElementById('site-navigation');
 
-    elmTitre.classList.add('anim1')
-    elmDescription.classList.add('anim3')
+    elmTitre.classList.add('anim1');
+    elmDescription.classList.add('anim3');
+    elmNav.classList.add('anim3');
 })
